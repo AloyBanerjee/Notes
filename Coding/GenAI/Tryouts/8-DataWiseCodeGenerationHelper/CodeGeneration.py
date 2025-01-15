5c214412667f6d8bf545fb2a1f0965f461ad5fb8
AloyBanerjee <aloy.banerjee30@gmail.com>
2025-01-15 08:28:17 +0530

    Upload

--- Coding/GenAI/Tryouts/8-DataWiseCodeGenerationHelper/CodeGeneration.py
+++ Coding/GenAI/Tryouts/8-DataWiseCodeGenerationHelper/CodeGeneration.py
@@ -15,7 +15,8 @@ from io import BytesIO
 from groq import Groq
 from e2b_code_interpreter import Sandbox
 from dotenv import load_dotenv
-
+#pip install streamlit-code-editor
+from code_editor import code_editor
 load_dotenv()
 
 warnings.filterwarnings("ignore", category=UserWarning, module="pydantic")
@@ -38,17 +39,6 @@ def code_interpret(e2b_code_interpreter: Sandbox, code: str) -> Optional[List[An
                 exec = e2b_code_interpreter.run_code(code)
                 #st.sidebar.info(exec)
 
-        # if stderr_capture.getvalue():
-        #     st.sidebar.error("[Code Interpreter Warnings/Errors]", file=sys.stderr)
-        #     st.sidebar.error(stderr_capture.getvalue(), file=sys.stderr)
-
-        # if stdout_capture.getvalue():
-        #     st.sidebar.error("[Code Interpreter Output]", file=sys.stdout)
-        #     st.sidebar.error(stdout_capture.getvalue(), file=sys.stdout)
-
-        # if exec.error:
-        #     st.sidebar.error(f"[Code Interpreter ERROR] {exec.error}", file=sys.stderr)
-        #     return None
         if stderr_capture.getvalue():
             st.sidebar.error("[Code Interpreter Warnings/Errors]")
             st.sidebar.error(stderr_capture.getvalue())  # No `file=sys.stderr` argument here.
@@ -73,8 +63,15 @@ def match_code_blocks(llm_response: str) -> str:
 def chat_with_llm(e2b_code_interpreter: Sandbox, user_message: str, dataset_path: str) -> Tuple[Optional[List[Any]], str]:
     # Update system prompt to include dataset path information
     system_prompt = f"""You're a Python data scientist and data visualization expert. You are given a dataset at path '{dataset_path}' and also the user's query.
-You need to analyze the dataset and answer the user's query with a response and you run Python code to solve them.
-IMPORTANT: Always use the dataset path variable '{dataset_path}' in your code when reading the CSV file."""
+        You need to analyze the dataset and answer the user's query with a response and you run Python code to solve them.
+        - the python code runs in jupyter notebook as well as in a streamlit app.
+        - every time you call `execute_python` tool, the python code is executed in a separate cell. it's okay to multiple calls to `execute_python`.
+        - display visualizations using matplotlib or any other visualization library directly in the notebook. don't worry about saving the visualizations to a file.
+        - you have access to the internet and can make api requests.
+        - you also have access to the filesystem and can read/write files.
+        - you can install any pip package (if it exists) if you need to but the usual packages for data analysis are already preinstalled.
+        - you can run any python code you want, everything is running in a secure sandbox environment
+        IMPORTANT: Always use the dataset path variable '{dataset_path}' in your code when reading the CSV file."""
 
     messages = [
         {"role": "system", "content": system_prompt},
@@ -94,11 +91,8 @@ IMPORTANT: Always use the dataset path variable '{dataset_path}' in your code wh
         
         #st.sidebar.info(python_code)
         
-        if python_code:
-            #st.sidebar.info('Coming Here!!')
-            code_interpreter_results = code_interpret(e2b_code_interpreter, python_code)
-            #st.sidebar.info(code_interpreter_results)
-            return code_interpreter_results, response_message.content
+        if python_code:            
+            return python_code, response_message.content
         else:
             st.warning(f"Failed to match any Python code in model's response")
             return None, response_message.content
@@ -113,10 +107,83 @@ def upload_dataset(code_interpreter: Sandbox, uploaded_file) -> str:
         st.error(f"Error during file upload: {error}")
         raise error
 
+def code_executor(e2b_code_interpreter, python_code, dataset_path, MODEL_NAME):
+  
+  MODEL_NAME = MODEL_NAME#"llama3-70b-8192"
+  client = Groq(api_key=st.session_state.groq_api_key) 
+  SYSTEM_PROMPT = """you are a python data scientist. you are given tasks to complete and you run python code to solve them.
+        - the python code runs in jupyter notebook and in streamlit app.
+        - every time you call `execute_python` tool, the python code is executed in a separate cell. it's okay to multiple calls to `execute_python`.
+        - display visualizations using matplotlib or any other visualization library directly in the notebook. don't worry about saving the visualizations to a file.
+        - you have access to the internet and can make api requests.
+        - you also have access to the filesystem and can read/write files.
+        - you can install any pip package (if it exists) if you need to but the usual packages for data analysis are already preinstalled.
+        - you can run any python code you want, everything is running in a secure sandbox environment"""
+  messages = [
+      {"role": "system", "content": SYSTEM_PROMPT}
+      
+  ] 
+  tools = [
+        {
+            "type": "function",
+            "function": {
+                "name": "execute_python",
+                "description": "Execute Python code generated by the 'explanation_problem' tool in a Streamlit app cell and return any result, stdout, stderr, display_data, and error.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "code": {
+                            "type": "string",
+                            "description": "The Python code to execute in a single cell."
+                        }
+                    },
+                    "required": ["code"]
+                }
+            }
+        }
+    ]
+
+  def code_interpret(e2b_code_interpreter, code):
+    st.write("Running code interpreter...")
+    exec = e2b_code_interpreter.run_code(code,
+    on_stderr=lambda stderr: print("[Code Interpreter]", stderr),
+    on_stdout=lambda stdout: print("[Code Interpreter]", stdout))
+    if exec.error:
+      print("[Code Interpreter ERROR]", exec.error)
+    else:
+      return exec.results
+  
+  
+  response = client.chat.completions.create(
+      model=MODEL_NAME,
+      messages=messages,
+      tools=tools,
+      tool_choice="auto",
+      max_tokens=4096,
+  )
+  response_message = response.choices[0].message
+  tool_calls = response_message.tool_calls
+  
+  if tool_calls:
+    for tool_call in tool_calls:
+      st.info(tool_call.function.name)
+      function_name = tool_call.function.name
+      function_args = json.loads(tool_call.function.arguments)
+      if function_name == "execute_python":
+        code = function_args["code"]
+        code_interpreter_results = code_interpret(e2b_code_interpreter, code)
+        return code_interpreter_results
+      else:
+        raise Exception(f"Unknown tool {function_name}")
+  else:
+    print(f"(No tool call in model's response) {response_message}")
+    return None
+
+
 
 def main():
     """Main Streamlit application."""
-    st.title("📊 AI Code Generation based on uploaded data")
+    st.title("📊 Python Code Generation based on uploaded data")
     st.write("Upload your dataset and ask questions about it!")
 
     # Initialize session state variables
@@ -147,14 +214,9 @@ def main():
             "Gemma 2 - 9b": "gemma2-9b-it",
             "Llama 3.3 Versatile - 70b": "llama-3.3-70b-versatile",
             "Llama 3.3 SpecDec - 70b": "llama-3.3-70b-specdec",
+            "Llama 3 8192 - 70b": "llama3-70b-8192",
             "Mixtral 8 - 7b": "mixtral-8x7b-32768"
         }
-        # model_options_together = {
-        #     "Meta-Llama 3.1 405B": "meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo",
-        #     "DeepSeek V3": "deepseek-ai/DeepSeek-V3",
-        #     "Qwen 2.5 7B": "Qwen/Qwen2.5-7B-Instruct-Turbo",
-        #     "Meta-Llama 3.3 70B": "meta-llama/Llama-3.3-70B-Instruct-Turbo"
-        # }
         st.session_state.model_name = st.selectbox(
             "Select Model",
             options=list(model_options.keys()),
@@ -175,8 +237,7 @@ def main():
             st.write("Preview (first 5 rows):")
             st.dataframe(df.head())
         # Query input
-        query = st.text_area("What would you like to know about your data?",
-                            "Can you compare the average cost for two people between different categories?")
+        query = st.text_area("What would you like to know about your data?")
         
         if st.button("Analyze"):
             if not st.session_state.groq_api_key or not st.session_state.e2b_api_key:
@@ -185,66 +246,39 @@ def main():
                 with Sandbox(api_key=st.session_state.e2b_api_key) as code_interpreter:
                     # Upload the dataset
                     dataset_path = upload_dataset(code_interpreter, uploaded_file)
+                    python_code, llm_response = chat_with_llm(code_interpreter, query, dataset_path)                   
                     
-                    # Pass dataset_path to chat_with_llm
-                    execution, llm_response = chat_with_llm(code_interpreter, query, dataset_path)
-                    
-                    # Display LLM's text response
-                    st.write("AI Response:")
-                    st.write(llm_response)
-                    st.write(execution.results)
-                    # Display results/visualizations
-
-                    for result in execution.results: 
-                        st.write('Coming!!!')
-                        st.write(result)
+                st.header("AI Response:")
+                st.write(llm_response)
 
 
-                    # if code_results:
-                    #     st.write('Coming!!!')
-                    #     # Access the 'Results' attribute which contains the list of Result objects
-                    #     #results = code_results.results  # This is where the list of Result objects is stored
-                    #     st.write(code_results.Logs.stdout)
-                    #     #st.dataframe(results[0].logs.stdout)  
-                    #     # # Iterate over each result in the list
-                    #     # for result in results:
-                    #     #     if hasattr(result, 'png') and result.png:  # Check if PNG data is available
-                    #     #         # Decode the base64-encoded PNG data
-                    #     #         png_data = base64.b64decode(result.png)
-                    #     #         with open('chart.png', 'wb') as f:
-                    #     #             f.write(base64.b64decode(png_data.png))
-                    #     #         # Convert PNG data to an image and display it
-                    #     #         image = Image.open(BytesIO(png_data))
-                    #     #         st.image(image, caption="Generated Visualization", use_container_width=False)
-                    #     #     if hasattr(result, 'figure'):  # For Matplotlib figures
-                    #     #         fig = result.figure  # Extract the Matplotlib figure
-                    #     #         if fig:  # Ensure the figure exists
-                    #     #             st.pyplot(fig)  # Display using st.pyplot
-                    #     #     if hasattr(result, 'show'):  # For Plotly figures
-                    #     #         st.plotly_chart(result)
-                    #     #     if isinstance(result.logs.stdout, (pd.DataFrame, pd.Series)):
-                    #     #         st.write('Coming Here!!')
-                    #     #         st.dataframe(result.logs.stdout)                          
-                    #     #     st.dataframe(result.logs.stdout)  
-
-                    # # if code_results:
-                    # #     for result in code_results:
-                    # #         if hasattr(result, 'png') and result.png:  # Check if PNG data is available
-                    # #             # Decode the base64-encoded PNG data
-                    # #             png_data = base64.b64decode(result.png)                                
-                    # #             # Convert PNG data to an image and display it
-                    # #             image = Image.open(BytesIO(png_data))
-                    # #             st.image(image, caption="Generated Visualization", use_container_width=False)
-                    # #         elif hasattr(result, 'figure'):  # For matplotlib figures
-                    # #             fig = result.figure  # Extract the matplotlib figure
-                    # #             if fig:  # Ensure the figure exists
-                    # #                 st.pyplot(fig)  # Display using st.pyplot
-                    # #         elif hasattr(result, 'show'):  # For plotly figures
-                    # #             st.plotly_chart(result)
-                    # #         elif isinstance(result, (pd.DataFrame, pd.Series)):
-                    # #             st.dataframe(result.logs.stdout)
-                    # #         else:
-                    # #             st.dataframe(result.logs.stdout)  
+                # with Sandbox(api_key=st.session_state.e2b_api_key) as code_interpreter:
+                #     code_results = code_executor(code_interpreter, python_code, dataset_path, st.session_state.model_name)
+                #     st.write('Execution completed')
+                #     st.write(code_results)
+                #     if code_results:
+                #         first_result = code_results[0]
+                #         st.sidebar.info(first_result)
+                #         #If there is an image
+                #         if hasattr(first_result, 'png') and first_result.png:  # Check if PNG data is available
+                #             # Decode the base64-encoded PNG data
+                #             png_data = base64.b64decode(first_result.png)
+                #             st.image(png_data, caption="Generated Visualization", use_container_width=False)  
+                #         #If there is a figure
+                #         if hasattr(first_result, 'figure'):  # For Matplotlib figures
+                #             fig = first_result.figure  # Extract the Matplotlib figure
+                #             if fig:  # Ensure the figure exists
+                #                 st.pyplot(fig)  # Display using st.pyplot
+                #         if hasattr(first_result, 'show'):  # For Plotly figures
+                #             st.plotly_chart(first_result)
+                #         #if there is a dataframe or series
+                #         if isinstance(first_result, (pd.DataFrame, pd.Series)):
+                #             st.dataframe(first_result)  
+                        
+                #     else:
+                #         st.info("No code results")
+                #         exit(0)
+                     
 
 if __name__ == "__main__":
     main()
